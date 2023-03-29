afaa360a41998b95c914f761a901b42a64aa6e62
kaede <kaede3256@gmail.com>
2023-03-29 21:38:31 +0900

    aaaa

--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from '../components/Layout/Layout';
+import Layout from '../components/Layout/Layout/Layout';
 import VideoDetail from '../components/Layout/VideoDetail/VideoDetail';
 
 const Watch = () => {
